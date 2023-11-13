07f9529f93bb5ae56956b3a33faabebf36844ba8
pak.to@tietoevry.com <pak.to@tietoevry.com>
2023-11-13 08:30:28 +0200

    muutokset 13.11.2023 part 2

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -13,7 +13,9 @@ namespace Engine
         public string Description { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
 
+        public Item RewardItem { get; set; }
         public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
             ID = id;
@@ -21,6 +23,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
 }
